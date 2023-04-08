f762e8eee5678d49504a7fb2cc1365142f675b77
Joe Huss <detain@interserver.net>
2023-04-07 20:27:36 -0400

    a bunch of work on the scoop updater code getting most of the powershell functins ironed out

--- scoop_finder.ps1
+++ scoop_finder.ps1
@@ -1,92 +1,98 @@
-function find($searchDir) {
-    Write-Host "Searching $searchDir ..."
-    $cmd = "Get-ChildItem $searchDir -Recurse -File"
-    $return = Invoke-Expression $cmd
-    Write-Host "done"
-    return $return
+$BucketNames = @()
+$LoadedBucketData = @{}
+$LoadedBucketName = ''
+$LoadedBuckets = @{}
+
+function Is-Numeric ($Value) {
+    return $Value -match "^[\d\.]+$"
 }
 
-class Bucket {
-    protected static $data;
+function Get-Bucket-ExtracteDir() {
+    $extractDir = Get-Bucket-Field('extract_dir', $true)
+    return $extractDir
+}
 
-    public static function load($bucket) {
-        $letter = $bucket.Substring(0, 1)
-        if ([char]::IsNumber($letter))
-            $letter = "#"
-        $fileName = "/mnt/e/dev/scoop-emulators/bucket/$letter/$bucket.json"
-        $data = Get-Content $fileName | ConvertFrom-Json
-        self::$data = $data
+function Get-Bucket-Urls() {
+    $urls = Get-Bucket-Field('extract_dir')
+    return $urls
+}
+
+function Get-Bucket-Bins() {
+    $bins = Get-Bucket-Field('extract_dir')
+    return $bins
+}
+
+function Load-Bucket ($Bucket) {
+    if ($LoadedBucketName -ne $Bucket) {
+        $letter = $Bucket.Substring(0, 1)
+        if (Is-Numeric $letter) {
+            $letter     = "#"
+            $fileName   = "/mnt/e/dev/scoop-emulators/bucket/$letter/$Bucket.json"
+            $data       = Get-Content $fileName | ConvertFrom-Json
+            $LoadedBucketData = $data
+            $LoadedBucketName = $Bucket
+            $LoadedBuckets[$LoadedBucketName] = $LoadedBucketData
+        }
     }
+    return $LoadedBucketData
+}
 
-    public static function getExtracteDir() {
-        $extractDir = self::getField('extract_dir', $true)
-        return $extractDir
-    }
-
-    public static function getUrls() {
-        $urls = self::getField('extract_dir')
-        return $urls
-    }
-
-    public static function getBins() {
-        $bins = self::getField('extract_dir')
-        return $bins
-    }
-
-    public static function getField($bucketFile, $field = 'bin', $lastOnly = $false) {
-        $data = Get-Content $bucketFile -Raw | ConvertFrom-Json
-        $return = @()
-        $sections = @(@(), @('architecture'), @('architecture', '32bit'), @('architecture', '64bit'))
-        foreach ($section in $sections) {
-            $var = $data
-            if ($section.Count -gt 0) {
-                foreach ($s in $section) {
-                    if ($var.$s) {
-                        $var = $var.$s
-                    } else {
-                        break
-                    }
-                }
-            }
-            if ($var.$field) {
-                $values = $var.$field
-                if (-not $values.GetType().IsArray) {
-                    $values = @($values)
-                }
-                foreach ($value in $values) {
-                    if ($value.GetType().IsArray) {
-                        $value = $value[0]
-                    }
-                    if (-not $return.Contains($value)) {
-                        $return += $value
-                    }
+function Get-Bucket-Field($bucketFile, $field = 'bin', $lastOnly = $false) {
+    $data = Load-Bucket($bucketFile)
+    $return = @()
+    $sections = @(@(), @('architecture'), @('architecture', '32bit'), @('architecture', '64bit'))
+    $sections | ForEach-Object {
+        $section = $_
+        $var = $data
+        if ($section.Count -gt 0) {
+            $section | ForEach-Object {
+                $s = $_
+                if ($var.$s) {
+                    $var = $var.$s
+                } else {
+                    break
                 }
             }
         }
-        if ($lastOnly) {
-            return $return[-1]
+        if ($var.$field) {
+            $values = $var.$field
+            if (-not $values.GetType().IsArray) {
+                $values = @($values)
+            }
+            $values | ForEach-Object {
+                $value = $_
+                if ($value.GetType().IsArray) {
+                    $value = $value[0]
+                }
+                if (-not $return.Contains($value)) {
+                    $return += $value
+                }
+            }
         }
-        return $return
     }
-
+    if ($lastOnly) {
+        return $return[-1]
+    }
+    return $return
 }
 
 function Get-BucketList {
     $buckets = @()
-    foreach ($fileName in Get-ChildItem '/mnt/e/dev/scoop-emulators/bucket/*/*.json') {
+    Get-ChildItem '/mnt/e/dev/scoop-emulators/bucket/*/*.json' | ForEach-Object {
+        $fileName = $_
         $buckets += [System.IO.Path]::GetFileNameWithoutExtension($fileName)
     }
     return $buckets
 }
 
-Invoke-WebRequest -Uri "https://github.com/detain/scoop-emulators/archive/refs/heads/master.zip" -OutFile "master.zip"
-Expand-Archive -Path "master.zip" -DestinationPath "./" -Force
-Remove-Item -Path "master.zip" -Force
-Get-ChildItem -Path "scoop-emulators-master/bucket" -Filter "*.json" -Recurse
+function Extract-Repo {
+    Invoke-WebRequest -Uri "https://github.com/detain/scoop-emulators/archive/refs/heads/master.zip" -OutFile "master.zip"
+    Expand-Archive -Path "master.zip" -DestinationPath "./" -Force
+    Remove-Item -Path "master.zip" -Force
+}
 
 
 $buckets = Get-BucketList
-
 
 
 $out = @{
@@ -97,8 +103,9 @@ $allBuckets = @{}
 $bin2bucket = @{}
 $regexes = @()
 $totalBins = 0
-foreach ($bucket in $buckets) {
-    [Bucket]::load($bucket)
+$buckets | ForEach-Object {
+    $bucket = $_
+    $BucketData = [Bucket]::load($bucket)
     $bins = [Bucket]::getBins()
     $urls = [Bucket]::getUrls()
     $extractDir = [Bucket]::getExtracteDir()
@@ -109,7 +116,8 @@ foreach ($bucket in $buckets) {
     if (-not [string]::IsNullOrWhiteSpace($extractDir)) {
         $allBuckets[$bucket]['extractDir'] = $extractDir
     }
-    foreach ($bin in $bins) {
+    $bins | ForEach-Object {
+        $bin = $_
         $totalBins++
         $bin = $bin.ToLower().Replace('\', '/')
         $regex = [regex]::Escape($bin)
@@ -121,32 +129,33 @@ foreach ($bucket in $buckets) {
         }
         $bin2bucket[$bin] += $bucket
     }
-    Write-Host "Bucket $bucket: $($bins -join ', ') $urls $extractDir"
+    Write-Output("Bucket {0}: {1} {2} {3}" -f $bucket, ($bins -join ", "), (Get-Bucket-Urls), (Get-Bucket-ExtracteDir))
 }
-if ($args.Count -lt 1) {
-    Write-Error "Invalid Syntax! $($MyInvocation.MyCommand.Name) <dir>"
-    return
-}
-$searchDir = $args[0]
-Write-Host "Found $totalBins Bins and $($bins.Count) Unique Bins to search for"
-$regexesPattern = '(' + [string]::Join('|', ($regexes | foreach { [regex]::Escape($_) })) + ')'
-$matches = Select-String -Path (Get-ChildItem -Path $searchDir -Recurse -Include $regexesPattern) -Pattern $regexesPattern -AllMatches | Select-Object -ExpandProperty Matches
-if ($matches.Count -eq 0) {
+
+#$searchDir = $args[0]
+$searchDir = "F:\Consoles\RetroBat"
+
+Write-Output "Found $totalBins Bins and $($bins.Count) Unique Bins to search for"
+$regexesPattern = '(' + [string]::Join('|', ($regexes | ForEach-Object { [regex]::Escape($_) })) + ')'
+$regexMatches = Select-String -Path (Get-ChildItem -Path $searchDir -Recurse -Include $regexesPattern) -Pattern $regexesPattern -AllMatches | Select-Object -ExpandProperty Matches
+if ($regexMatches.Count -eq 0) {
     Write-Error "No matches were found"
     Exit 1
 }
 
 $fileEmus = @{}
-foreach ($match in $matches) {
-    $fileName = $match.Value
-    $bin = $match.Groups[1].Value.ToLower()
+$regexMatches | ForEach-Object {
+    $regexMatch = $_
+    $fileName = $regexMatch.Value
+    $bin = $regexMatch.Groups[1].Value.ToLower()
     if (-not $bin2bucket.ContainsKey($bin)) {
-        Write-Host "Cant find $bin in: $($bin2bucket.Keys -join ', ')"
+        Write-Output "Cant find $bin in: $($bin2bucket.Keys -join ', ')"
     } else {
         $dirName = Split-Path $fileName -Parent
         $fileEmus[$dirName] = $bin2bucket[$bin]
         $out.paths[$dirName] = $fileEmus[$dirName]
-        foreach ($emu in $bin2bucket[$bin]) {
+        $bin2bucket[$bin] | ForEach-Object {
+            $emu = $_
             $out.emus[$emu] = $allBuckets[$emu]
         }
     }
