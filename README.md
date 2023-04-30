ff0bfd91e5ffa463500ebe998c3cdcaaa5c06f21
Joe Huss <detain@interserver.net>
2023-04-30 16:48:32 -0400

    updates to readme and removig some old crap

--- README.md
+++ README.md
@@ -24,15 +24,15 @@ Searches through directories for known emulator executable files, lets you custo
 * [x] Get Bins
 * [x] Display Matches
 * [x] remove repo on close
-* [ ] fix database group element distribution/sizing
+* [x] fix database group element distribution/sizing
 * [ ] fix logo display / scaling
 * [ ] add check all button to discovered emulators
-* [ ] fix width on disco emulators sections
-* [ ] add saving/loading discovered/emulators and such
+* [x] fix width on disco emulators sections
+* [x] add saving/loading discovered/emulators and such
 * [ ] change code around to avoid globals
 * [ ] Ensure all bins are returned
 * [ ] use caching so you dont reaload the data every time you click an emulator
-* [ ] better extractDir handling
+* [x] better extractDir handling
 * [ ] Upgrade Emulators
 * [x] Auto-Resize Elements to match window size
 * [ ] progress bar/status while scanning
--- scoop_finder.ps1
+++ scoop_finder.ps1
@@ -9,7 +9,7 @@ $global:bin2buckets = @{}
 $global:regexes = @()
 $global:totalBins = 0
 
-function Get-Emulator {
+function Update-Emulator {
     [CmdletBinding()]
     param(
         [Parameter(Mandatory=$true)]
@@ -82,75 +82,6 @@ function Get-Emulator {
         }
     }
 }
-
-
-#  foreach ($emuId in $data.emus.Keys) {
-#     $emuData = $data.emus[$emuId]
-#     $baseDir = "$PSScriptRoot\new\$emuId"
-#     $moveDir = "$baseDir\extracted"
-#     $finalDir = "$baseDir\final"
-
-#     if (!(Test-Path $finalDir)) {
-#         Write-Output "Emulator $emuId"
-#         New-Item -ItemType Directory -Path $moveDir -Force | Out-Null
-#         Write-Output "  Downloading $($emuData.urls)"
-#         Invoke-WebRequest -Uri "https://github.com/detain/scoop-emulators/archive/refs/heads/master.zip" -OutFile "master.zip"
-#         Expand-Archive -Path "master.zip" -DestinationPath ".\" -Force
-#         Remove-Item -Path "master.zip" -Force
-
-#         $resultCode = cmd /c "wget --max-redirect 100 -c $($emuData.urls) -O `"$($baseDir)\$($emuData.urls | Split-Path -Leaf)`""
-#         if ($resultCode -ne 0) {
-#             $results += "dl $emuId"
-#         }
-#         Write-Output "Got Exit Code $resultCode"
-#         Write-Output "  Extracting $($emuData.urls)"
-#         if ($emuData.urls -match '\.tar\.(xz|bz2|gz)$') {
-#             $resultCode = cmd /c "tar -C `"$moveDir`" -xvf `"$($baseDir)\$($emuData.urls | Split-Path -Leaf)`""
-#         }
-#         else {
-#             $resultCode = cmd /c "7z x -bb0 -aoa -o`"$moveDir`" `"$($baseDir)\$($emuData.urls | Split-Path -Leaf)`""
-#         }
-#         if ($resultCode -ne 0) {
-#             $results += "7z $emuId"
-#         }
-#         Write-Output "Got Exit Code $resultCode"
-#         if ($emuData.extractDir -and (Test-Path "$moveDir\$($emuData.extractDir)")) {
-#             $moveDir += "\$($emuData.extractDir)"
-#         }
-#         Move-Item $moveDir $finalDir
-#         $results
-#     }
-# }
-
-
-
-function Install-Emulator() {
-    foreach ($emuPath in $data.paths.Keys) {
-        $emus = $data.paths[$emuPath]
-        if ($emus.Count -ne 1) {
-            Write-Output "Wrong count of emus ($($emus -join ','))"
-            continue
-        }
-        $emuId = $emus[0]
-        $emuData = $data.emus[$emuId]
-        $baseDir = "$PSScriptRoot\new\$emuId"
-        $backupZip = "$baseDir\backup_$emuId_$($emuPath -replace '\', '_')_$(Get-Date -Format yyyyMMdd).zip"
-        $finalDir = "$baseDir\final"
-        if ($doBackup && !Test-Path $backupZip) {
-            Write-Output "  Backing up to $backupZip";
-            $escapedBackupZip = [System.Management.Automation.WildcardPattern]::Escape($backupZip)
-            $escapedEmuPath = [System.Management.Automation.WildcardPattern]::Escape($emuPath)
-            & 'zip.exe' '-r' $escapedBackupZip $escapedEmuPath | Write-Output
-        }
-        if (Test-Path $finalDir) {
-            Write-Output "Emulator $emuId $finalDir => $emuPath"
-            $cmd = "cp -afv `"$finalDir\*`" `"$emuPath\`""
-            Write-Output $cmd
-            cmd /c $cmd
-        }
-    }
-}
-
 
 function Expand-Repo {
     Invoke-WebRequest -Uri "https://github.com/detain/scoop-emulators/archive/refs/heads/master.zip" -OutFile "master.zip"
@@ -310,11 +241,3 @@ function Find-Emu-Matches {
     }
     return $global:pathMatches;
 }
-
-#Expand-Repo
-#Get-BucketCollection
-#Read-BucketCollection
-#Find-Bucket-Matches
-#$out = Find-Emu-Matches
-#$out | ConvertTo-Json -Depth 10 | Set-Content -Path 'found_emulators.json'
-#Remove-Item -Path scoop-emulators-master -Recurse -Force
